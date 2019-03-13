008edce0f3c99d768cba1cbcafe6b4d8164f4a1e
swolebrain <thecodingteacher@gmail.com>
2019-03-13 10:10:55 -0400

    feat: footer is done

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,18 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import {StaticQuery, graphql} from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+//images
+import spiral from '../images/footer_bg_spirals.svg';
+import phone from '../images/footer-phone.svg';
+
+const Layout = ({children}) => (
+    <StaticQuery
+        query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -23,31 +27,29 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+        render={data => (
+            <>
+                <Header siteTitle={data.site.siteMetadata.title}/>
+                <div
+                    style={{
+                        margin: `0 auto`,
+                        maxWidth: 960,
+                        padding: `0px 1.0875rem 1.45rem`,
+                        paddingTop: 0,
+                    }}
+                >
+                    <main>{children}</main>
+                </div>
+                <footer>
+                    <img src={phone} alt="get your mobile app from us" className={"footer__phone"}/>
+                </footer>
+            </>
+        )}
+    />
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
---
+++ src/pages/resources.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+// export default function ResourcesPage(props){
+//
+// }
+
+export default class ResourcesPage extends React.Component {
+    state = {
+        todos: [],
+        counter: 1
+    }
+
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){}//disparan network requests
+    shouldComponentUpdate(nextProps, nextState){} //para hacer optimizaciones de render - debo volver a hacer render? true/false
+    componentDidUpdate(prevProps, prevState, snapshot){}
+    increaseCounter = () => {
+        this.setState({
+            counter: this.state.counter+1
+        });
+    }
+    addTodo = () => {
+        this.setState(prevState => ({
+                todos: [...prevState.todos, window.prompt("Enter your todo")]
+            }));
+    }
+    render(){
+        return (
+            <div>
+                <h1>Your counter is <span>{this.state.counter}</span></h1>
+                <button onClick={this.increaseCounter}>Increase Counter</button>
+                {
+                    this.state.todos.length > 0 && <h1>todos:</h1>
+                }
+                {
+                    this.state.todos.map((elemento, indice) =>
+                        <h3 key={indice}>{elemento}</h3>
+                    )
+                }
+                <button onClick={this.addTodo}>Add Todo</button>
+            </div>
+        );
+    }
+}
+
+/*
+  true && "hello" => "hello"
+  false && "hello" => false
+
+  undefined || "hello" => "hello"
+  undefined || ()=>"hello" => fn
+ */
+
