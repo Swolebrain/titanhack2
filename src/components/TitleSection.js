fc568fc602635aae6ef5f6851d378d851162e438
nicokurogane <fyedream@gmail.com>
2019-03-21 14:48:57 -0600

    Minor class name adjusment to Title Section

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 import "./titlesection.css"
-import "../fonts/Montserrat/montserrat.css"
+
 
 export default function TitleSection  ({title, backgroundTitle}){
     return(    
-    <div className={"container"}>
-        <div className={"box text-background"}>  <h3 className={"text"}>{backgroundTitle}</h3>    </div>
-        <div className={"box-title text-foreground"}>    <h3 className={"text"}>{title} </h3> <span className="line"></span>       </div>
+    <div className={"section-title-container"}>
+        <div className={"section-title-box text-background"}>  <h3 className={"text"}>{backgroundTitle}</h3>    </div>
+        <div className={"section-title-box-title text-foreground"}>    <h3 className={"text"}>{title} </h3> <span className="line"/> </div>
     </div>
 )}
