db86e73ca596d9c1c19c47dce50856b33569b5c4
nicokurogane <fyedream@gmail.com>
2019-04-29 14:32:16 -0600

    feat: added more keyframes  to 3D-cube

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import {StaticQuery, graphql} from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
@@ -19,7 +19,7 @@ import FooterTriangle from "../images/footer-bg-42.svg";
 
 
 
-const Layout = ({children}) => (
+const Layout = ({ children }) => (
     <StaticQuery
         query={graphql`
       query SiteTitleQuery {
@@ -32,24 +32,24 @@ const Layout = ({children}) => (
     `}
         render={data => (
             <>
-                <Header siteTitle={data.site.siteMetadata.title}/>
+                <Header siteTitle={data.site.siteMetadata.title} />
                 <div>
                     <main>{children}</main>
                 </div>
                 <div className={"footer-container"}>
-                <img src={FooterTriangle} className={"footer-triangle"}/>
-                <footer className={"footer-bg"}> 
-                    <img src={phone} alt="get your mobile app from us" className={"footer__phone"}/>
-                <div className={"footer-subsection"}>
-                    <div className={"question"}><big><h3>Do you have a project in mind?</h3></big></div>
-                        <div className={"button-sct"}><a href="" className="btn-lets-talk">Talk with us!</a>
-                            <div className={"footer-subsection-social-links"}>
-                                <a target="_blank" href="https://www.youtube.com/thecodingteacher"><img src={YouTubeImg} className={"social-link"} /></a>
-                                 <a href=""><img src={YouTubeImg} className={"social-link"} /></a>
+                    <img src={FooterTriangle} className={"footer-triangle"} />
+                    <footer className={"footer-bg"}>
+                        <img src={phone} alt="get your mobile app from us" className={"footer__phone"} />
+                        <div className={"footer-subsection"}>
+                            <div className={"question"}><big><h3>Do you have a project in mind?</h3></big></div>
+                            <div className={"button-sct"}><a href="" className="btn-lets-talk">Talk with us!</a>
+                                <div className={"footer-subsection-social-links"}>
+                                    <a target="_blank" href="https://www.youtube.com/thecodingteacher"><img src={YouTubeImg} className={"social-link"} /></a>
+                                    <a href=""><img src={YouTubeImg} className={"social-link"} /></a>
+                                </div>
                             </div>
                         </div>
-                </div>
-                </footer>
+                    </footer>
                 </div>
             </>
         )}
