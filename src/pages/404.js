c999890ea194fbb75bc15384ee7be2c74a2e2725
nicokurogane <fyedream@gmail.com>
2019-05-01 07:57:17 -0600

    fix: fixed some images position on 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 
-import PcHero from '../images/comingsoon.svg';
+import CommingSoonImg from '../images/comingsoon.svg';
 import "../components/404.css"
 
 const NotFoundPage = () => (
@@ -13,7 +13,7 @@ const NotFoundPage = () => (
       
       <Header siteTitle="titanhack"/>
       <div className={"main-container"}>
-        <img src={PcHero} alt="pc hero" className={"working-img"}/> 
+        <img src= {CommingSoonImg} alt="pc hero" className={"working-img"}/> 
         <h1>Coming Soon</h1>
         <span className={"paragraph-subtitle"}>Titanhack is currently working hard building this page !</span>
         <a href="/" className="btn-work btn-shadow">Back To Home</a>
