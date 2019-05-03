94bf5f6190da442c068f6dc1c5382feedfcafb9b
nicokurogane <fyedream@gmail.com>
2019-05-03 14:38:22 -0600

    fix: 404 page now adjust to viewport's height

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,19 +6,20 @@ import "../components/404.css"
 
 const NotFoundPage = () => (
   <>
-      <div className={"ornament ornament-top-left"} />
-      <div className={"ornament ornament-middle-left"} />
-      <div className={"ornament ornament-middle-right"} />
-      <div className={"ornament ornament-bottom-right"} />
-      
-      <Header siteTitle="titanhack"/>
+    
+    <div className="container-404">
+    <div className={"ornament ornament-top-left"} />
+    <div className={"ornament ornament-middle-left"} />
+    <div className={"ornament ornament-middle-right"} />
+    <div className={"ornament ornament-bottom-right"} />
+      <Header siteTitle="titanhack" />
       <div className={"main-container"}>
-        <img src= {CommingSoonImg} alt="pc hero" className={"working-img"}/> 
+        <img src={CommingSoonImg} alt="pc hero" className={"working-img"} />
         <h1>Coming Soon</h1>
         <span className={"paragraph-subtitle"}>Titanhack is currently working hard building this page !</span>
         <a href="/" className="btn-work btn-shadow">Back To Home</a>
       </div>
-      
+    </div>
   </>
 )
 
